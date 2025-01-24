c8eaf2affa24193648672eed9bc02a9877e42936
PARASMANI-KHUNTE <parasmanikhunte@gmail.com>
2025-01-24 21:59:00 +0530

    added local url

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: "https://admin-dashboard-js6u.onrender.com"
+  origin: ["https://admin-dashboard-js6u.onrender.com", "http://localhost:5173"]
 }));
 // Routes
 app.use('/api/admin', adminRoutes);
